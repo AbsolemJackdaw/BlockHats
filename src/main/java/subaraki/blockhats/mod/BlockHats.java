4f9e937d3ce40f1783e843a8ee0dfd022cecd2a1
ArtixAllMighty <robbeenaxel@hotmail.com>
2016-07-30 18:43:04 +0200

    mod file and mod info

---
+++ src/main/java/subaraki/blockhats/mod/BlockHats.java
@@ -0,0 +1,19 @@
+package subaraki.blockhats.mod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = BlockHats.MODID, version = BlockHats.VERSION)
+public class BlockHats
+{
+
+	public static final String MODID = "BlockHats";
+	public static final String VERSION = "1.0";
+
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+
+	}
+}
